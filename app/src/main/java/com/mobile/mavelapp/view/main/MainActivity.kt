f67800831a8a3bc7c681d24b8c408c71bf4d17bc
Igor Vieira <igor_recreio23@hotmail.com>
2019-07-28 12:42:33 -0300

    [Marvel-0010] add progress loading and recycle view

--- app/src/main/java/com/mobile/mavelapp/view/main/MainActivity.kt
+++ app/src/main/java/com/mobile/mavelapp/view/main/MainActivity.kt
@@ -3,10 +3,12 @@ package com.mobile.mavelapp.view.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.mobile.mavelapp.R
 import com.mobile.mavelapp.injection.presenterModelResolver
 import com.mobile.mavelapp.model.Result
 import com.mobile.mavelapp.presenter.MainPresenter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ViewInterface {
 
@@ -25,9 +27,14 @@ class MainActivity : AppCompatActivity(), ViewInterface {
     }
 
     override fun requestSuccess(results: ArrayList<Result>) {
+        dimissProgressBar()
         results.forEach(){
             Log.e("IGOR",it.name + " " + it.id)
         }
+    }
+
+    fun dimissProgressBar() {
+        pbLoading.visibility = View.GONE
     }
 
 }
