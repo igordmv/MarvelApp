0292d58c0426b430f010d53fce34d5dd9592d3eb
Igor Vieira <igor_recreio23@hotmail.com>
2019-07-31 23:27:15 -0300

    [Marvel-0038] add whenClickOnRecycleViewItemShouldGoToHeroDetailActivity espresso test

--- app/src/androidTest/java/com/mobile/mavelapp/view/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/mobile/mavelapp/view/main/MainActivityTest.kt
@@ -1,26 +1,38 @@
 package com.mobile.mavelapp.view.main
 
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.util.TreeIterables
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
 import com.mobile.mavelapp.R
+import com.mobile.mavelapp.view.detail.HeroDetailActivity
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
 import org.hamcrest.Matchers.not
+import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.util.concurrent.CountDownLatch
 
 
 @RunWith(JUnit4::class)
 @LargeTest
 class MainActivityTest {
 
-    @Rule
-    @JvmField
-    val activity = ActivityTestRule(MainActivity::class.java)
+    @get:Rule
+    val activity = IntentsTestRule(MainActivity::class.java)
 
     @Test
     fun whenClickOnSearchIconShouldShowRecycleSearchedAndHideFirstRecycle() {
@@ -32,4 +44,61 @@ class MainActivityTest {
         onView(withId(R.id.recycler)).check(matches(not(isDisplayed())))
     }
 
+    @Test
+    fun whenClickOnRecycleViewItemShouldGoToHeroDetailActivity(){
+        Assert.assertTrue(viewExists(allOf(withId(R.id.ivHeroList),withEffectiveVisibility(Visibility.VISIBLE)),5000)) //wait 5 seconds
+        onView(withId(R.id.recycler)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, clickOnViewChild(R.id.ivHeroList)))
+        intended(hasComponent(HeroDetailActivity::class.java!!.getName()))
+    }
+
+    fun clickOnViewChild(viewId: Int) = object : ViewAction {
+        override fun getConstraints() = null
+
+        override fun getDescription() = "Click on a child view with specified id."
+
+        override fun perform(uiController: UiController, view: View) = click().perform(uiController, view.findViewById<View>(viewId))
+    }
+
+    @Throws(InterruptedException::class)
+    fun viewExists(viewMatcher: Matcher<View>, millis: Long): Boolean {
+        val found = arrayOfNulls<Boolean>(1)
+
+        val latch = CountDownLatch(1)
+        val action = object : ViewAction {
+            override fun getConstraints(): Matcher<View> {
+                return isRoot()
+            }
+
+            override fun getDescription(): String {
+                return "wait for a specific view with id <$viewMatcher> during $millis millis."
+            }
+
+            override fun perform(uiController: UiController, view: View) {
+                uiController.loopMainThreadUntilIdle()
+                val startTime = System.currentTimeMillis()
+                val endTime = startTime + millis
+
+
+                do {
+                    for (child in TreeIterables.breadthFirstViewTraversal(view)) {
+
+                        if (viewMatcher.matches(child)) {
+                            found[0] = true
+                            latch.countDown()
+                            return
+                        }
+                    }
+
+                    uiController.loopMainThreadForAtLeast(50)
+                } while (System.currentTimeMillis() < endTime)
+
+                found[0] = false
+                latch.countDown()
+            }
+        }
+        onView(isRoot()).perform(action)
+
+        latch.await()
+        return found[0]!!
+    }
 }
